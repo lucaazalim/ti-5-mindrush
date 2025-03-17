374492bcc3bbd6a64566caa1cd187da18a5573ab
Luca Azalim <lucaazalim@gmail.com>
2025-03-16 23:00:23 -0300

    web: instalando e configurando NextAuth.

--- code/web/src/server/db/schema.ts
+++ code/web/src/server/db/schema.ts
@@ -1,10 +1,8 @@
-import { sql } from "drizzle-orm";
+import { relations, sql } from "drizzle-orm";
 import {
   boolean,
   index,
   integer,
-  pgEnum,
-  pgTable,
   pgTableCreator,
   primaryKey,
   text,
@@ -13,8 +11,104 @@ import {
   uuid,
   varchar,
 } from "drizzle-orm/pg-core";
+import { type AdapterAccount } from "next-auth/adapters";
 
+/**
+ * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
+ * database instance for multiple projects.
+ *
+ * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
+ */
 export const createTable = pgTableCreator((name) => `mindrush_${name}`);
+
+export const users = createTable("user", {
+  id: varchar("id", { length: 255 })
+    .notNull()
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  name: varchar("name", { length: 255 }),
+  email: varchar("email", { length: 255 }).notNull(),
+  emailVerified: timestamp("email_verified", {
+    mode: "date",
+    withTimezone: true,
+  }).default(sql`CURRENT_TIMESTAMP`),
+  image: varchar("image", { length: 255 }),
+});
+
+export const usersRelations = relations(users, ({ many }) => ({
+  accounts: many(accounts),
+}));
+
+export const accounts = createTable(
+  "account",
+  {
+    userId: varchar("user_id", { length: 255 })
+      .notNull()
+      .references(() => users.id),
+    type: varchar("type", { length: 255 })
+      .$type<AdapterAccount["type"]>()
+      .notNull(),
+    provider: varchar("provider", { length: 255 }).notNull(),
+    providerAccountId: varchar("provider_account_id", {
+      length: 255,
+    }).notNull(),
+    refresh_token: text("refresh_token"),
+    access_token: text("access_token"),
+    expires_at: integer("expires_at"),
+    token_type: varchar("token_type", { length: 255 }),
+    scope: varchar("scope", { length: 255 }),
+    id_token: text("id_token"),
+    session_state: varchar("session_state", { length: 255 }),
+  },
+  (account) => ({
+    compoundKey: primaryKey({
+      columns: [account.provider, account.providerAccountId],
+    }),
+    userIdIdx: index("account_user_id_idx").on(account.userId),
+  }),
+);
+
+export const accountsRelations = relations(accounts, ({ one }) => ({
+  user: one(users, { fields: [accounts.userId], references: [users.id] }),
+}));
+
+export const sessions = createTable(
+  "session",
+  {
+    sessionToken: varchar("session_token", { length: 255 })
+      .notNull()
+      .primaryKey(),
+    userId: varchar("user_id", { length: 255 })
+      .notNull()
+      .references(() => users.id),
+    expires: timestamp("expires", {
+      mode: "date",
+      withTimezone: true,
+    }).notNull(),
+  },
+  (session) => ({
+    userIdIdx: index("session_user_id_idx").on(session.userId),
+  }),
+);
+
+export const sessionsRelations = relations(sessions, ({ one }) => ({
+  user: one(users, { fields: [sessions.userId], references: [users.id] }),
+}));
+
+export const verificationTokens = createTable(
+  "verification_token",
+  {
+    identifier: varchar("identifier", { length: 255 }).notNull(),
+    token: varchar("token", { length: 255 }).notNull(),
+    expires: timestamp("expires", {
+      mode: "date",
+      withTimezone: true,
+    }).notNull(),
+  },
+  (vt) => ({
+    compoundKey: primaryKey({ columns: [vt.identifier, vt.token] }),
+  }),
+);
 
 // Create enums
 // const questionTypeEnum = pgEnum("question_type", ["QUIZ", "TRUE_OR_FALSE"]);
@@ -26,7 +120,7 @@ export const createTable = pgTableCreator((name) => `mindrush_${name}`);
 // ]);
 
 // Tables
-export const educator = pgTable("educator", {
+export const educator = createTable("educator", {
   id: uuid("id").primaryKey().defaultRandom(),
   name: text("name").notNull(),
   email: text("email").notNull().unique(),
@@ -36,7 +130,7 @@ export const educator = pgTable("educator", {
     .default(sql`CURRENT_TIMESTAMP`),
 });
 
-export const quiz = pgTable("quiz", {
+export const quiz = createTable("quiz", {
   id: uuid("id").primaryKey().defaultRandom(),
   educatorId: uuid("educator_id")
     .notNull()
@@ -48,7 +142,7 @@ export const quiz = pgTable("quiz", {
     .default(sql`CURRENT_TIMESTAMP`),
 });
 
-export const question = pgTable("question", {
+export const question = createTable("question", {
   id: uuid("id").primaryKey().defaultRandom(),
   quizId: uuid("quiz_id")
     .notNull()
@@ -61,19 +155,22 @@ export const question = pgTable("question", {
     .default(sql`CURRENT_TIMESTAMP`),
 });
 
-export const questionQuizAlternatives = pgTable("question_quiz_alternatives", {
-  id: uuid("id").primaryKey().defaultRandom(),
-  questionId: uuid("question_id")
-    .notNull()
-    .references(() => question.id),
-  answer: text("answer").notNull(),
-  correct: boolean("correct").notNull(),
-  createdAt: timestamp("created_at")
-    .notNull()
-    .default(sql`CURRENT_TIMESTAMP`),
-});
+export const questionQuizAlternatives = createTable(
+  "question_quiz_alternatives",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+    questionId: uuid("question_id")
+      .notNull()
+      .references(() => question.id),
+    answer: text("answer").notNull(),
+    correct: boolean("correct").notNull(),
+    createdAt: timestamp("created_at")
+      .notNull()
+      .default(sql`CURRENT_TIMESTAMP`),
+  },
+);
 
-export const match = pgTable("match", {
+export const match = createTable("match", {
   id: uuid("id").primaryKey().defaultRandom(),
   quizId: uuid("quiz_id")
     .notNull()
@@ -85,7 +182,7 @@ export const match = pgTable("match", {
     .default(sql`CURRENT_TIMESTAMP`),
 });
 
-export const participant = pgTable(
+export const participant = createTable(
   "participant",
   {
     id: uuid("id").primaryKey().defaultRandom(),
@@ -99,7 +196,7 @@ export const participant = pgTable(
   }),
 );
 
-export const quizAnswer = pgTable(
+export const quizAnswer = createTable(
   "quiz_answer",
   {
     id: uuid("id").primaryKey().defaultRandom(),
