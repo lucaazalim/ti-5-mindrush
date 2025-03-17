bf7d6ac4e33f9173b491121ed1e42450da50c281
Luca Azalim <lucaazalim@gmail.com>
2025-03-16 23:47:14 -0300

    web: adicionando autenticação com Google.

--- code/web/src/app/page.tsx
+++ code/web/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
+import SignInButton from "./_components/SignInButton";
 
 export default async function HomePage() {
   return (
     <main>
       <p>Mindrush!</p>
+      <SignInButton />
     </main>
   );
 }
