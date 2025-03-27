b2ba04cf90945e3401e53e0999eee36b7107acbb
oarthurfc <arthuryouface@gmail.com>
2025-03-27 16:24:22 -0300

    web: Atualiza modelagem do Quiz para receber novos atributos
    #25

--- code/web/src/server/quiz.ts
+++ code/web/src/server/quiz.ts
@@ -5,11 +5,32 @@ import { db } from "./db";
 import { quiz } from "./db/schema";
 import { eq } from "drizzle-orm";
 
+export type QuizInput = {
+  educatorId: string;
+  title: string;
+  description: string;
+  type: "BLANK" | "AI_GENERATED" | "PDF_GENERATED";
+  theme?: string;
+  difficulty?: "EASY" | "MEDIUM" | "HARD";
+  language?: string;
+  pdfBase64?: string;
+};
+
+type QuizUpdateInput = {
+  title?: string;
+  description?: string;
+};
+
 const quizIdSchema = z.string().uuid();
 const quizSchema = z.object({
   educatorId: z.string().uuid(),
   title: z.string().min(3, "O título deve ter pelo menos 3 caracteres"),
-  description: z.string().min(10, "A descrição deve ter pelo menos 10 caracteres"),
+  description: z.string().min(8, "A descrição deve ter pelo menos 10 caracteres"),
+  type: z.enum(["BLANK", "AI_GENERATED", "PDF_GENERATED"]),
+  theme: z.string().optional(), 
+  difficulty: z.enum(["EASY" , "MEDIUM" , "HARD"]).optional(), 
+  language: z.string().optional(), 
+  pdfBase64: z.string().optional(), 
 });
 
 export async function getAllQuizzes() {
@@ -27,12 +48,8 @@ export async function getQuizById(id: string) {
   });
 }
 
-export async function createQuiz(
-  educatorId: string,
-  title: string,
-  description: string
-) {
-  const parsedData = quizSchema.safeParse({ educatorId, title, description });
+export async function createQuiz(quizData: QuizInput) {
+  const parsedData = quizSchema.safeParse(quizData);
   if (!parsedData.success) {
     throw new Error("Dados inválidos para criação do quiz");
   }
@@ -40,11 +57,7 @@ export async function createQuiz(
   return await db.insert(quiz).values(parsedData.data);
 }
 
-export async function updateQuiz(
-  id: string,
-  title?: string,
-  description?: string
-) {
+export async function updateQuiz(id: string, updateData: QuizUpdateInput) {
   const parsedId = quizIdSchema.safeParse(id);
   if (!parsedId.success) {
     throw new Error("ID inválido");
@@ -52,10 +65,10 @@ export async function updateQuiz(
 
   const updateSchema = z.object({
     title: z.string().min(3).optional(),
-    description: z.string().min(10).optional(),
+    description: z.string().min(8).optional(),
   });
 
-  const parsedUpdate = updateSchema.safeParse({ title, description });
+  const parsedUpdate = updateSchema.safeParse(updateData);
   if (!parsedUpdate.success) {
     throw new Error("Dados inválidos para atualização do quiz");
   }
