e46cfdc6cc4625719edd8cc85a6260d21802a8ed
Luca Azalim <lucaazalim@gmail.com>
2025-04-07 16:48:25 -0300

    web: tornando middleware mais robusto e seguro.

--- code/web/src/lib/constants.ts
+++ code/web/src/lib/constants.ts
@@ -1,4 +1,15 @@
-export const ROUTES = {
+export type Route = `/${string}`;
+
+export const ROUTES: {
+  API: Route;
+  HOME: Route;
+  DASHBOARD: Route;
+  QUIZZES: Route;
+  QUIZ: (id: string) => Route;
+  MATCHES: Route;
+  MATCH: (id: string) => Route;
+} = {
+  API: "/api",
   HOME: "/",
   DASHBOARD: "/dashboard",
   QUIZZES: "/dashboard/quizzes",
--- code/web/src/middleware.ts
+++ code/web/src/middleware.ts
@@ -1,11 +1,41 @@
 import { auth } from "~/server/auth";
 import { ROUTES } from "~/lib/constants";
+import { NextResponse } from "next/server";
+import { Route } from "next";
 
-export default auth((req) => {
-  if (req.nextUrl.pathname.startsWith(ROUTES.DASHBOARD) && !req.auth) {
-    const homeUrl = new URL(ROUTES.HOME, req.nextUrl.origin);
+export default auth((request) => {
+  // This always return the pathname WITH leading slash and WITHOUT trailing slash.
+  const pathname = request.nextUrl.pathname;
+
+  // This makes sure "/api" route does not match "/apiculture" pathname, for example.
+  function matchesRoute(route: Route) {
+    return (pathname + "/").startsWith(route + "/");
+  }
+
+  // Redirect to home if the user is not authenticated and trying to access the dashboard
+  if (matchesRoute(ROUTES.DASHBOARD) && !request.auth) {
+    const homeUrl = new URL(ROUTES.HOME, request.nextUrl.origin);
     return Response.redirect(homeUrl);
   }
+
+  // CORS
+  const response = NextResponse.next();
+
+  if (matchesRoute(ROUTES.API)) {
+    response.headers.set("Access-Control-Allow-Origin", "*");
+
+    response.headers.set(
+      "Access-Control-Allow-Methods",
+      "GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS",
+    );
+
+    response.headers.set(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization",
+    );
+  }
+
+  return response;
 });
 
 export const config = {
