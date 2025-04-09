1a526f7101f1a745d64090399976caa9eaacb893
Luca Azalim <lucaazalim@gmail.com>
2025-04-09 15:08:30 -0300

    web: adicionando estado "loading" ao button.tsx.

--- code/web/src/components/ui/button.tsx
+++ code/web/src/components/ui/button.tsx
@@ -1,8 +1,9 @@
 import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
+import {Slot, Slottable} from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "~/lib/utils"
+import {LoaderCircle} from "lucide-react";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
@@ -10,15 +11,15 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+          "bg-primary text-primary-foreground shadow hover:bg-primary/90 data-[loader=true]:[&>svg]:!text-primary-foreground",
         destructive:
-          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
+          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90 data-[loader=true]:[&>svg]:!text-destructive-foreground",
         outline:
-          "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
+          "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground data-[loader=true]:[&>svg]:!text-accent-foreground",
         secondary:
-          "bg-secondary text-background shadow-sm hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+          "bg-secondary text-background shadow-sm hover:bg-secondary/80 data-[loader=true]:[&>svg]:!text-secondary-foreground",
+        ghost: "hover:bg-accent hover:text-accent-foreground data-[loader=true]:[&>svg]:!text-accent-foreground",
+        link: "text-primary underline-offset-4 hover:underline data-[loader=true]:[&>svg]:!text-primary",
       },
       size: {
         default: "rounded-3xl py-3 px-8",
@@ -36,23 +37,51 @@ const buttonVariants = cva(
 )
 
 export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
-  asChild?: boolean
+    extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+        VariantProps<typeof buttonVariants> {
+    asChild?: boolean;
+    loading?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button"
-    return (
-      <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
-        ref={ref}
-        {...props}
-      />
-    )
-  }
-)
-Button.displayName = "Button"
+    (
+        {
+            className,
+            variant,
+            size,
+            disabled,
+            loading = false,
+            children,
+            asChild = false,
+            ...props
+        },
+        ref,
+    ) => {
+        const Comp = asChild ? Slot : "button";
 
-export { Button, buttonVariants }
+        return (
+            <Comp
+                className={cn(
+                    buttonVariants({
+                        variant,
+                        size,
+                        className,
+                    }),
+                    {
+                        "select-none text-transparent": loading,
+                    },
+                )}
+                ref={ref}
+                disabled={loading || disabled}
+                {...props}
+            >
+                {loading && <LoaderCircle data-loader={true} className="absolute animate-spin" />}
+                <Slottable>{children}</Slottable>
+            </Comp>
+        );
+    },
+);
+
+Button.displayName = "Button";
+
+export { Button, buttonVariants };
