235939c371fd3841e80307a8a5ddbb14bfa46367
Luca Azalim <lucaazalim@gmail.com>
2025-04-09 14:35:33 -0300

    web: ajustando implementação da página de partida.

--- code/web/src/app/dashboard/matches/[id]/page.tsx
+++ code/web/src/app/dashboard/matches/[id]/page.tsx
@@ -1,54 +1,53 @@
-import QRCodePage from "./_components/QRCodePage";
-import {getMatchByIdOrPin} from "~/server/actions/match-actions";
-import {isFailure} from "~/lib/result";
+import { getMatchByIdOrPin } from "~/server/actions/match-actions";
+import { isFailure } from "~/lib/result";
+import QRCode from "qrcode";
+import Image from "next/image";
 
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const match = await getMatchByIdOrPin(id);
 
-export default async function Page(
-    { params }: { params: Promise<{ id: string }> },
-) {
-    console.log("Inicio do componente")
-    const {id} = await params;
-    const match = await getMatchByIdOrPin(id);
+  // TODO: Handle error
+  if (isFailure(match)) {
+    return <p>error</p>;
+  }
 
-    // TODO: Handle error
-    if (isFailure(match)) {
-        return <p>error</p>;
-    }
+  const pin = match.data.pin;
 
-    const pin = match.data.pin;
+  // TODO não consegui solucionar o problema abaixo sem usar eslint-disable-next-line.
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-assignment
+  const qrCodeBase64 = await QRCode.toDataURL(pin);
 
-    console.log("match",match);
+  const participants = [];
+  const hasPlayers = participants.length > 0;
 
-    /*// Pin
-    const searchParams = useSearchParams();
-    const pin = searchParams.get("pin");*/
+  const withPlayer = participants.length;
+  const withoutPlayer = (
+    <h2 className="border-2xl mt-4 w-96 rounded-lg bg-purple-500 p-4 text-center text-background">
+      Aguardando jogadores...
+    </h2>
+  );
 
-    const listDeJagadores = []
-
-    const isPlayer = listDeJagadores.length > 0;
-
-    const withPlayer = listDeJagadores.length;
-    const withoutPlayer = <h2 className="text-white w-96 mt-4 bg-purple-500 border-2xl rounded-lg p-4 mt-4 text-center">
-        Aguardando jogadores...
-    </h2>;
-
-    return (
-        <div className="flex flex-col items-center justify-center w-full h-screen">
-            {/* Pin e qrcode */}
-            <div className="flex justify-center w-full gap-4">
-                <div className="bg-white rounded-lg py-2 px-16 flex flex-col items-start justify-center">
-                    <h1 className="">PIN do Jogo</h1>
-                    <h2 className="font-bold text-5xl">{pin}</h2>
-                </div>
-                <div className="max-w-96 ">
-                    <QRCodePage text={pin} />
-                </div>
-
-            </div>
-
-            <div>
-                { isPlayer ? withPlayer : withoutPlayer }
-            </div>
+  return (
+    <div className="flex w-full flex-col items-center justify-center">
+      <div className="flex w-full justify-center gap-4">
+        <div className="flex flex-col items-start justify-center rounded-lg bg-background px-16 py-2">
+          <h1 className="">PIN da partida</h1>
+          <h2 className="text-5xl font-bold">{pin}</h2>
         </div>
-    )
+        <div className="max-w-96">
+          <div className="flex flex-col items-center gap-4 rounded-lg border p-4 shadow-md">
+            <h2 className="text-lg font-semibold">QR Code</h2>
+            <Image src={qrCodeBase64} alt="QR Code" width={160} height={160} />
+          </div>
+        </div>
+      </div>
+
+      <div>{hasPlayers ? withPlayer : withoutPlayer}</div>
+    </div>
+  );
 }
