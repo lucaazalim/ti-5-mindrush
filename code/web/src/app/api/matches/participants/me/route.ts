00b25873344d25b1ccb27a78d13a942a3dc592d2
Luca Azalim <lucaazalim@gmail.com>
2025-05-04 19:43:11 -0300

    web: renomenado rota GET /api/matches/participants para GET /api/matches/participants/me.

--- code/web/src/app/api/matches/participants/me/route.ts
+++ code/web/src/app/api/matches/participants/me/route.ts
@@ -2,7 +2,7 @@ import { NextRequest } from "next/dist/server/web/spec-extension/request";
 import { NextResponse } from "next/server";
 import { isFailure } from "~/lib/result";
 import { Participant } from "~/lib/types";
-import { APIError, apiErrorResponse, authParticipant } from "../../api";
+import { APIError, apiErrorResponse, authParticipant } from "../../../api";
 
 export async function GET(req: NextRequest): Promise<NextResponse<Participant | APIError>> {
   const participant = await authParticipant(req);
