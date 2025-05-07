5e22b1b317dd429e6612c88326bd72a6b5fc0223
oarthurfc <arthuryouface@gmail.com>
2025-05-07 11:32:59 -0300

    feat: adiciona links de navegação na navbar

--- code/web/src/app/dashboard/_components/header/Header.tsx
+++ code/web/src/app/dashboard/_components/header/Header.tsx
@@ -1,19 +1,38 @@
 import Image from "next/image";
 import ProfileDropdownMenu from "./ProfileDropdownMenu";
 import ThemeSwitcherButton from "./ThemeSwitchButton";
+import Link from "next/link";
+import { ROUTES } from "~/lib/constants";
 
 export default async function Header() {
   return (
-    <nav className="bg-primary dark:bg-background">
+    <div className="bg-primary dark:bg-background">
       <div className="mx-auto flex max-w-5xl flex-row justify-between p-5">
-        <div className="my-auto">
+        <div className="my-auto flex items-center">
           <Image src="/logo.png" alt="Logo do MindRush" width={100} height={0} />
         </div>
+        <nav className="flex mx-auto w-1/3 items-center justify-between text-white font-bold">
+            <Link href={ROUTES.HOME}>
+              <p>
+                Home
+              </p>
+            </Link>
+            <Link href={ROUTES.QUIZZES}>
+              <p>
+                Quizzes
+              </p>
+            </Link>
+            <Link href={ROUTES.MATCHES}>
+              <p>
+                Partidas
+              </p>
+            </Link>
+          </nav>
         <div className="flex flex-row items-center gap-5">
           <ThemeSwitcherButton />
           <ProfileDropdownMenu />
         </div>
       </div>
-    </nav>
+    </div>
   );
 }
