ff5cf9e927ea6999e3432fd99feed6d0f8ae8be8
Luca Azalim <lucaazalim@gmail.com>
2025-05-26 16:02:29 -0300

    web: corrigindo CWE 352.
    https://github.com/ICEI-PUC-Minas-PPLES-TI/plf-es-2025-1-ti5-0492100-mindrush/issues/64

--- code/web/src/app/(home)/_components/SignInButton.tsx
+++ code/web/src/app/(home)/_components/SignInButton.tsx
@@ -1,20 +1,18 @@
+"use client";
+
+import { signIn } from "next-auth/react";
 import { Button } from "~/components/ui/button";
-import { signIn } from "~/lib/auth";
 import { ROUTES } from "~/lib/constants";
 
 export default function SignInButton() {
   return (
-    <form
-      action={async () => {
-        "use server";
-        await signIn("google", {
-          redirectTo: ROUTES.DASHBOARD,
-        });
-      }}
+    <Button
+      className="bg-primary text-[15px]"
+      size="default"
+      type="button"
+      onClick={() => signIn("google", { callbackUrl: ROUTES.DASHBOARD })}
     >
-      <Button className="bg-primary text-[15px]" size="default" type="submit">
-        Acessar
-      </Button>
-    </form>
+      Acessar
+    </Button>
   );
 }
