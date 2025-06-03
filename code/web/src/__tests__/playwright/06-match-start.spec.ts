2bbd1b99ec91d5822fd727987e598c12a4774078
Jhonata S. Dias <118060258+jhonatasdias@users.noreply.github.com>
2025-06-03 16:49:41 -0300

    web: adicionando CT6 #87
    - ICEI-PUC-Minas-PPLES-TI/plf-es-2025-1-ti5-0492100-mindrush/issues/87

--- code/web/src/__tests__/playwright/06-match-start.spec.ts
+++ code/web/src/__tests__/playwright/06-match-start.spec.ts
@@ -1,10 +1,35 @@
-// import { test } from "@playwright/test";
-// import { addSessionTokenCookie } from "./utils";
+import { test, expect } from "@playwright/test";
+import { addSessionTokenCookie } from "./utils";
 
-// test("rename quiz from the dashboard", async ({ page, context }) => {
-//   await test.step("Authenticate user via session cookie", async () => {
-//     await addSessionTokenCookie(context);
-//   });
+test("start match from dashboard", async ({ page, context }) => {
+  await test.step("Authenticate user via session cookie", async () => {
+    await addSessionTokenCookie(context);
+  });
 
-//   // TODO
-// });
+  await test.step("Navigate to quizzes dashboard", async () => {
+    await page.goto("/dashboard/quizzes");
+    await expect(page).toHaveURL("/dashboard/quizzes");
+  });
+
+  await test.step("Click track match button", async () => {
+    await page.getByRole("button", { name: "Acompanhar partida" }).first().click();
+  });
+
+  await test.step("Expect redirection to match page", async () => {
+    await expect(page).toHaveURL(/\/dashboard\/matches\/[a-fA-F0-9-]{36}$/);
+  });
+
+  await test.step("Click start match button", async () => {
+    const button = page.getByRole("button", { name: /Iniciar partida/i });
+
+    // Garante que o botão está habilitado (deve ter participantes no match store)
+    await expect(button).toBeEnabled();
+
+    // Clica para iniciar partida
+    await button.click();
+
+    // Espera por efeito da mutation (toast, loading, etc)
+    await page.waitForTimeout(500); // ou aguarde um seletor
+
+  });
+});
